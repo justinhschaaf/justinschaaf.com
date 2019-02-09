cdc95be8ea8dbffb78826dd57d31ca16b6cc61b9
Justin Schaaf <justinhschaaf@outlook.com>
2019-02-08 21:12:52 -0800

    Begin adding content
    Home page & projects

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+
+const routes: Routes = [
+  
+  {
+    path: '',
+    component: HomeComponent
+  },
+
+  {
+    path: 'home',
+    redirectTo: '',
+    pathMatch: 'full'
+  },
+
+  {
+    path: 'projects',
+    component: ProjectsComponent
+  },
+
+  {
+    path: 'project',
+    redirectTo: '',
+    pathMatch: 'full'
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
