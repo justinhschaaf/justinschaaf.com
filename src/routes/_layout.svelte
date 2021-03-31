a578197ceea98f1935a16163d34031a570302369
Justin Schaaf <justinhschaaf@outlook.com>
2021-03-31 13:25:57 -0700

    Release Day Patch 2
    - Attempt to fix broken link colors and header font by adding "!important" to styles
    - If this doesn't work, I don't care right now. I'll fix it later.......

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,5 +1,21 @@
 <style lang="scss">
-	@import "global.scss";
+
+	* {
+		font-family: 'Overpass Mono', 'Segoe UI Emoji', sans-serif;
+	}
+
+	h1, h2, h3, h4, h5, h6 {
+		font-family: 'Major Mono Display', 'Segoe UI Emoji', monospace !important;
+	}
+
+	a {
+		color: #777777 !important;
+	}
+
+	img {
+		width: 100%;
+	}
+
 </style>
 
 <main>
