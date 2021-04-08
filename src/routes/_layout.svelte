99b674d659ed2ba41ea05cc4d3ee176af060405e
Justin Schaaf <justinhschaaf@outlook.com>
2021-04-08 11:36:44 -0700

    This should fix the problems, and if it doesn't I'll be very mad
    - Wrap all global styles in `:global()` to force them to be applied globally when placed in `_layout.svelte`. This should fix header fonts not working and footer link colors.

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,18 +1,29 @@
 <style lang="scss">
 
-	* {
+	/*
+	 * Reference: https://github.com/sveltejs/sapper/issues/562#issuecomment-463542668
+	 * 
+	 * So apparently removes styles it considers "unnecessary" and does not recognize the
+	 * nested header tags in this component's subcomponents with no way I know of to disable it. 
+	 * :global() forces these styles to be applied globally, but it's still stupid I have to
+	 * include such a tag instead of these styles being applied to all children elements. It's
+	 * also stupid that I can't just wrap all the styles in a :global() {}-like tag with SCSS,
+	 * but at least it's better than having to import a SCSS file into every single component.
+	 */
+
+	:global(*) {
 		font-family: 'Overpass Mono', 'Segoe UI Emoji', sans-serif;
 	}
 
-	h1, h2, h3, h4, h5, h6 {
-		font-family: 'Major Mono Display', 'Segoe UI Emoji', monospace !important;
+	:global(h1, h2, h3, h4, h5, h6) {
+		font-family: 'Major Mono Display', 'Segoe UI Emoji', monospace;
 	}
 
-	a {
-		color: #777777 !important;
+	:global(a) {
+		color: #777777;
 	}
 
-	img {
+	:global(img) {
 		width: 100%;
 	}
 
