f2c225fb9b0abf8e1f578bc35f26a56f8ddc8258
Justin Schaaf <justinhschaaf@outlook.com>
2024-03-15 03:19:24 -0700

    fix: make pages for disabled projects inaccessible

--- src/routes/projects/[slug]/+page.server.ts
+++ src/routes/projects/[slug]/+page.server.ts
@@ -1,10 +1,20 @@
 import type { PageServerLoad } from './$types';
 import * as content from '$lib/content';
+import { error } from '@sveltejs/kit';
 
 // Yes, we have to use Svelte's fetch or it complains
 // https://kit.svelte.dev/docs/load#making-fetch-requests
 export const load: PageServerLoad = async ({ fetch, params }) => {
+
+    let project = await content.fetchProject(fetch, params.slug);
+
+    // Make sure disabled projects are inaccessible
+    if (project.disabled) {
+        error(404);
+    }
+
     return {
-        project: await content.fetchProject(fetch, params.slug)
+        project: project
     };
+
 };
