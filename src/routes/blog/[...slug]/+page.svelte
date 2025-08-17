f7668438efb3b7678ad301dc8f86a1d001552e09
Justin Schaaf <justin@justinschaaf.com>
2025-08-16 23:47:27 -0700

    feat: add read time to blog posts

--- src/routes/blog/[...slug]/+page.svelte
+++ src/routes/blog/[...slug]/+page.svelte
@@ -7,8 +7,23 @@
     // Determine the created and updated dates here
     let created: number = page.data.post.data.created;
     let updated: number = ("updated" in page.data.post.data) ? page.data.post.data.updated : -1;
+
+    // Calculate the time to read
+    let words: number = page.data.post.content.split(" ").length;
+    let time_to_read = Math.round(words / 175);
     
 </script>
+
+<style lang="scss">
+
+    .infobar {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: nowrap;
+        justify-content: space-between;
+    }
+
+</style>
 
 <!-- ***METADATAHHHH!!!!!*** -->
 <svelte:head>
@@ -26,20 +41,25 @@
 
 </svelte:head>
 
+<div class="infobar">
+    <PubDate {created} {updated}/>
+    <span>{time_to_read} min read</span>
+</div>
+
+<hr><br>
+
 <Markdown>
     
     <!-- Warnings if you're looking at a hidden or disabled post -->
     {#if page.data.post.data.hidden}
 
-    {"> [!IMPORTANT]\n"}
-    {"> This post is hidden and difficult to reach. You're special though, so you can still read it.\n\n"}
+    {"> [!IMPORTANT]\n> This post is hidden and difficult to reach. You're special though, so you can still read it.\n\n"}
 
     {/if}
 
     {#if page.data.post.data.disabled}
 
-    {"> [!CAUTION]\n"}
-    {"> This post has been disabled. How the fuck did you get here?\n\n"}
+    {"> [!CAUTION]\n> This post has been disabled. How the fuck did you get here?\n\n"}
 
     {/if}
 
@@ -47,7 +67,3 @@
     {page.data.post.content}
 
 </Markdown>
-
-<br><hr>
-
-<PubDate {created} {updated}/>
