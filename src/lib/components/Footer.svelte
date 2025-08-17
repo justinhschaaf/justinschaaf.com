dd4472fd3037fe86b3ba9fba9b77d5b22e85bcbf
Justin Schaaf <justin@justinschaaf.com>
2025-08-16 19:21:24 -0700

    feat: add training data disclaimer to footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -4,15 +4,6 @@
 
     import type { SocialIcon } from "$lib/content";
     import { darkTheme } from "$lib/theme";
-
-    
-
-    // version info
-
-    
-
-    
-
     
     interface Props {
         /** An object containing the social icons to display */
@@ -34,8 +25,7 @@
 
     let socialKeys: string[] = (socials != null) ? Object.keys(socials) : [];
 
-
-    function toggleTheme(event: MouseEvent) {
+    function toggleTheme() {
         $darkTheme = !$darkTheme;
     }
 
@@ -49,71 +39,85 @@
         color: var(--color-light);
         box-shadow: var(--shadow);
 
-        display: flex;
-        flex-direction: row;
-        flex-wrap: wrap;
-        justify-content: center;
-
         // Makes sure it's on top of any fixed elements (*cough cough* two-pane image on project pages)
         position: sticky;
         z-index: 690 !important;
 
-        .footcontent {
-
-            min-width: 240px;
-            padding: var(--padding-large);
-            
-            // Remove bottom padding so when it's stacked there's not excessive extra space
-            // #cursedcsstricks
-            padding-bottom: 0;
+        .prefoot {
 
             display: flex;
-            flex-direction: column;
+            flex-direction: row;
+            flex-wrap: wrap;
+            justify-content: center;
 
-            // Always something new to learn about flexbox https://stackoverflow.com/a/29503264
-            flex: 1 1 240px;
+            .footcontent {
 
-            h2 {
-                margin: 0 0 var(--padding-small) 0;
-            }
+                min-width: 240px;
+                padding: var(--padding-large);
 
-            .socialbox {
+                // Remove bottom padding so when it's stacked there's not excessive extra space
+                // #cursedcsstricks
+                padding-bottom: 0;
 
                 display: flex;
-                flex-direction: row;
-                flex-wrap: nowrap;
+                flex-direction: column;
 
-                .social {
+                // Always something new to learn about flexbox https://stackoverflow.com/a/29503264
+                flex: 1 1 240px;
 
-                    width: var(--padding-large);
-                    height: var(--padding-large);;
-                    margin: 0 var(--padding-small) 0 0;
+                h2 {
+                    margin: 0 0 var(--padding-small) 0;
+                }
+
+                .socialbox {
 
                     display: flex;
-                    flex-direction: column;
-                    justify-content: center;
+                    flex-direction: row;
+                    flex-wrap: nowrap;
 
-                    img {
-                        width: calc(var(--padding-large) / 2);
-                        color: #ffffff !important;
+                    .social {
+
+                        width: var(--padding-large);
+                        height: var(--padding-large);;
+                        margin: 0 var(--padding-small) 0 0;
+
+                        display: flex;
+                        flex-direction: column;
+                        justify-content: center;
+
+                        img {
+                            width: calc(var(--padding-large) / 2);
+                            color: #ffffff !important;
+                        }
+
                     }
 
                 }
 
             }
 
-        }
-
-        .footcontent-right {
-            padding: var(--padding-large);
-        }
-
-        // magic value
-        @media screen and (min-width: 816px) {
-            .footcontent-right {
-                align-items: flex-end;
-                text-align: right;
+            // magic value
+            @media screen and (min-width: 816px) {
+                .footcontent-right {
+                    align-items: flex-end;
+                    text-align: right;
+                }
             }
+
+        }
+
+        .postfoot {
+
+            display: flex;
+            flex-direction: row;
+            flex-wrap: wrap;
+            justify-content: center;
+
+            .postfootcontent {
+                padding: var(--padding-large);
+                opacity: 0.1;
+            }
+
         }
 
     }
@@ -176,68 +180,76 @@
 -->
 <footer class="foot">
 
-    <div class="footcontent">
+    <div class="prefoot">
 
-        <h2>Socials</h2>
+        <div class="footcontent">
 
-        <div class="socialbox">
+            <h2>Socials</h2>
 
-            {#each socialKeys as key}
-                
-                {#if !socials[key].disabled}
+            <div class="socialbox">
 
-                    <a class="social foothover" href={socials[key].url} target="_blank">
-                        
-                        <img src={socials[key].icon} alt={socials[key].name}>
+                {#each socialKeys as key}
 
-                    </a>
+                    {#if !socials[key].disabled}
 
-                {/if}
+                        <a class="social foothover" href={socials[key].url} target="_blank">
 
-            {/each}
+                            <img src={socials[key].icon} alt={socials[key].name}>
+
+                        </a>
+
+                    {/if}
+
+                {/each}
+
+            </div>
+
+        </div>
+
+        <div class="theme">
+
+            <svg class="wire" viewBox="0 0 48 58">
+                <line x1="24" y1="0" x2="24" y2="58" stroke="#fff" stroke-width="2px"/>
+            </svg>
+
+            <!-- This has to be a button for accessibility -->
+            <button onclick={toggleTheme} class="bulb foothover">
+                <!-- That's Omori -->
+                <img alt="A lightbulb used to change the website theme."
+                     src={$darkTheme ? "/assets/icons/theme_dark.svg" : "/assets/icons/theme_light.svg"}
+                     title={$darkTheme
+                    ? "A broken lightbulb hangs from the ceiling. Do you want to replace it?"
+                    : "A lightbulb hangs from the ceiling. Do you want to destroy it?"}>
+            </button>
+
+        </div>
+
+        <div class="footcontent footcontent-right">
+
+            <h2>Source</h2>
+
+            <!--
+                https://kit.svelte.dev/docs/modules#$env-dynamic-private
+                https://github.com/search?q=repo%3Aactions%2Fdeploy-pages+process.env&type=code
+                https://docs.github.com/en/actions/learn-github-actions/variables#default-environment-variables
+                https://stackoverflow.com/questions/53648652/how-to-use-environment-variables-in-github-page
+            -->
+
+            <span>{repo}</span>
+
+            <!-- Only show commit in production -->
+            {#if dev}
+                <span>Development Server</span>
+            {:else}
+                <span>{ref}@<a href="https://github.com/{repo}/tree/{sha}" target="_blank">{sha?.substring(0, 7)}</a></span>
+            {/if}
 
         </div>
 
     </div>
 
-    <div class="theme">
-
-        <svg class="wire" viewBox="0 0 48 58">
-            <line x1="24" y1="0" x2="24" y2="58" stroke="#fff" stroke-width="2px"/>
-        </svg>
-
-        <!-- This has to be a button for accessibility -->
-        <button onclick={toggleTheme} class="bulb foothover">
-            <!-- That's Omori -->
-            <img alt="A lightbulb used to change the website theme."
-                src={$darkTheme ? "/assets/icons/theme_dark.svg" : "/assets/icons/theme_light.svg"} 
-                title={$darkTheme 
-                    ? "A broken lightbulb hangs from the ceiling. Do you want to replace it?"
-                    : "A lightbulb hangs from the ceiling. Do you want to destroy it?"}>
-        </button>
-
-    </div>
-
-    <div class="footcontent footcontent-right">
-
-        <h2>Source</h2>
-
-        <!-- 
-            https://kit.svelte.dev/docs/modules#$env-dynamic-private 
-            https://github.com/search?q=repo%3Aactions%2Fdeploy-pages+process.env&type=code
-            https://docs.github.com/en/actions/learn-github-actions/variables#default-environment-variables
-            https://stackoverflow.com/questions/53648652/how-to-use-environment-variables-in-github-page
-        -->
-
-        <span>{repo}</span>
-
-        <!-- Only show commit in production -->
-        {#if dev}
-            <span>Development Server</span>
-        {:else}
-            <span>{ref}@<a href="https://github.com/{repo}/tree/{sha}" target="_blank">{sha?.substring(0, 7)}</a></span>
-        {/if}
-
+    <div class="postfoot">
+        <span class="postfootcontent">You may not use any content on this site as training data.</span>
     </div>
     
 </footer>
